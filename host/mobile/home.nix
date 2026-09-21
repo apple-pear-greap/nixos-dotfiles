3330d417c37c2eb7f56b798bc7808ba4215fb4f1
Cerydra <cerydrahysilens@qq.com>
2026-09-21 10:39:36 +0800

    add intel graphic config

--- host/mobile/home.nix
+++ host/mobile/home.nix
@@ -6,11 +6,19 @@
   ...
 }:
 let
-  nvimPath = "/home/yuan/nix-config/config/nvim";
+  configPath = "${config.home.homeDirectory}/nixos-config/config";
+  creatSymlink = path: config.lib.file.mkOutOfStoreSymlink path;
+  configs = {
+    nvim = "nvim";
+    rofi = "rofi";
+    foot = "foot";
+    sway = "sway";
+  };
 in
 {
   imports = [
     ../../home-modules/yazi.nix
+    inputs.helium-flake.homeModules.default
   ];
   home.username = "yuan";
   home.homeDirectory = "/home/yuan";
@@ -27,6 +35,11 @@ in
     gamescope
     bluetui
     spotify
+
+    foot
+    swaybg
+    waybar
+
     pkg-config
   ];
   home.sessionVariables = {
@@ -34,11 +47,29 @@ in
   };
   programs.kitty = {
     enable = true;
+    themeFile = "Dracula";
+    font = {
+      name = "JetBrainsMonoNL NFP";
+      size = 14;
+    };
+    settings = {
+      cursor_trail = 1;
+      cursor_trail_decay = "0.1 0.3";
+      cursor_trail_start_threshold = 3;
+
+      background_opacity = 0.8;
+    };
   };
 
   programs.firefox.enable = true;
   programs.chromium = {
     enable = true;
+  };
+  programs.helium = {
+    enable = true;
+    flags = [
+      "--ozone-platform-hint=auto"
+    ];
   };
   programs.git = {
     enable = true;
@@ -47,10 +78,12 @@ in
       user.email = "cerydrahysilens@qq.com";
     };
   };
+
   programs.bash = {
     enable = true;
     shellAliases = {
       nrs = "sudo nixos-rebuild switch";
+      zc = "cd ~/nixos-config/";
     };
   };
 
@@ -73,10 +106,10 @@ in
     ];
   };
 
-  xdg.configFile."nvim" = {
-    source = config.lib.file.mkOutOfStoreSymlink "/home/yuan/nixos-config/config/nvim";
+  xdg.configFile = builtins.mapAttrs (name: subpath: {
+    source = creatSymlink "${configPath}/${subpath}";
     recursive = true;
-  };
+  }) configs;
 
   home.stateVersion = "26.05";
 }
